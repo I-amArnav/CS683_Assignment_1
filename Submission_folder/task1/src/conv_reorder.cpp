a207f9574f7308cd046161be23908899bca9eb10
I-amArnav <baranwalarnav.8106@gmail.com>
2026-08-30 10:24:58 +0000

    First drafts of reorder, tiling, unrolling

--- Submission_folder/task1/src/conv_reorder.cpp
+++ Submission_folder/task1/src/conv_reorder.cpp
@@ -6,5 +6,29 @@
 void conv_reorder(const float* in, float* out, const float* ker,
                   int H, int W, int K) {
     // TODO(student): replace this placeholder with your reordered implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+
+    for (int ky = 0; ky < K; ++ky) {
+        for (int kx = 0; kx < K; ++kx) {
+            float kval = ker[ky * K + kx];
+            if(kx==0 && ky==0){
+                for (int oy = 0; oy < H; ++oy) {
+                    const float* in_row = in + (oy + ky) * in_stride + kx;
+                    float* out_row = out + oy * W;
+                    for (int ox = 0; ox < W; ++ox) {
+                        out_row[ox] = in_row[ox] * kval;
+                    }
+                }
+                continue;
+            }
+            for (int oy = 0; oy < H; ++oy) {
+                const float* in_row = in + (oy + ky) * in_stride + kx;
+                float* out_row = out + oy * W;
+                for (int ox = 0; ox < W; ++ox) {
+                    out_row[ox] += in_row[ox] * kval;
+                }
+            }
+        }
+    }
 }
--- Submission_folder/task1/src/conv_unroll.cpp
+++ Submission_folder/task1/src/conv_unroll.cpp
@@ -4,5 +4,45 @@
 void conv_unroll(const float* in, float* out, const float* ker,
                  int H, int W, int K) {
     // TODO(student): replace this placeholder with your unrolled implementation.
-    conv_naive(in, out, ker, H, W, K);
+    const int p = K / 2;
+    const int in_stride = W + 2 * p;  // padded row stride
+
+    for (int ky = 0; ky < K; ++ky) {
+        for (int kx = 0; kx < K; ++kx) {
+            float kval = ker[ky * K + kx];
+            if(kx==0 && ky==0){
+                for (int oy = 0; oy < H; ++oy) {
+                    const float* in_row = in + (oy + ky) * in_stride + kx;
+                    float* out_row = out + oy * W;
+                    int ox = 0;
+                    for (; ox < W; ox += 8) {
+                        out_row[ox + 0] = in_row[ox + 0] * kval;
+                        out_row[ox + 1] = in_row[ox + 1] * kval;
+                        out_row[ox + 2] = in_row[ox + 2] * kval;
+                        out_row[ox + 3] = in_row[ox + 3] * kval;
+                        out_row[ox + 4] = in_row[ox + 4] * kval;
+                        out_row[ox + 5] = in_row[ox + 5] * kval;
+                        out_row[ox + 6] = in_row[ox + 6] * kval;
+                        out_row[ox + 7] = in_row[ox + 7] * kval;
+                    }
+                }
+                continue;
+            }
+            for (int oy = 0; oy < H; ++oy) {
+                const float* in_row = in + (oy + ky) * in_stride + kx;
+                float* out_row = out + oy * W;
+                int ox = 0;
+                for (; ox < W; ox += 8) {
+                    out_row[ox + 0] += in_row[ox + 0] * kval;
+                    out_row[ox + 1] += in_row[ox + 1] * kval;
+                    out_row[ox + 2] += in_row[ox + 2] * kval;
+                    out_row[ox + 3] += in_row[ox + 3] * kval;
+                    out_row[ox + 4] += in_row[ox + 4] * kval;
+                    out_row[ox + 5] += in_row[ox + 5] * kval;
+                    out_row[ox + 6] += in_row[ox + 6] * kval;
+                    out_row[ox + 7] += in_row[ox + 7] * kval;
+                }
+            }
+        }
+    }
 }
