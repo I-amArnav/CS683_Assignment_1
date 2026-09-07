081764884cb919437ffc92e2836caafa73280087
namishBansal <namishbansal28@gmail.com>
2026-09-07 12:14:41 +0530

    Change tile size in conv_tile function

--- Submission_folder/task1/src/conv_tile.cpp
+++ Submission_folder/task1/src/conv_tile.cpp
@@ -5,13 +5,13 @@ void conv_tile(const float* in, float* out, const float* ker,
                int H, int W, int K) {
     const int p = K / 2;
     const int in_stride = W + 2 * p;  // padded row stride
-    const int TILE_Y = 64; 
-    const int TILE_X = 64;
+    const int tile_y = 16; 
+    const int tile_x = 256;
 
-    for (int ty = 0; ty < H; ty += TILE_Y) {
-        int max_oy = std::min(ty + TILE_Y, H);        
-        for (int tx = 0; tx < W; tx += TILE_X) {
-            int max_ox = std::min(tx + TILE_X, W);
+    for (int ty = 0; ty < H; ty += tile_y) {
+        int max_oy = std::min(ty + tile_y, H);        
+        for (int tx = 0; tx < W; tx += tile_x) {
+            int max_ox = std::min(tx + tile_x, W);
             for (int oy = ty; oy < max_oy; ++oy) {
                 for (int ox = tx; ox < max_ox; ++ox) {
                     float acc = 0.0f;
